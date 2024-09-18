e364b286d44faff48b5ba216a9e04b6758f7295d
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2024-09-18 16:18:07 +0100

    Add security scan

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.5")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.6")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-web")
