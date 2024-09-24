f650394c742a3837175d78b93c6f1682eb34d037
Bingjie Liu <Bingjie.Liu@justice.gov.uk>
2024-09-24 15:12:33 +0100

    Fix merge issues (#11)
    * Fix merge issues
    
    * Fix merge issues
    
    ---------
    
    Co-authored-by: Bingjie Liu <bingjie.liu@digital.justice.gov.uk>

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,19 +9,19 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.6")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.7")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.4.3")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.4.4")
   implementation("io.sentry:sentry-spring-boot-starter-jakarta:7.14.0")
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
   runtimeOnly("org.postgresql:postgresql:42.7.2")
   testImplementation("com.h2database:h2:2.3.232")
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.0.5")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.0.7")
   testImplementation("org.wiremock:wiremock-standalone:3.9.1")
 }
 
