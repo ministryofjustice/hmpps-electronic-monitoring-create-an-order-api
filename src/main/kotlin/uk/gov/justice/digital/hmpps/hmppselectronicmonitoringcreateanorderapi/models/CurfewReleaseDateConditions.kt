ec86309aed0f8194fa9fb7ecd7ea2b50f133a4be
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2024-10-22 16:01:20 +0100

    Implement CurfewReleaseDateController

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/CurfewReleaseDateConditions.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/CurfewReleaseDateConditions.kt
@@ -2,10 +2,16 @@ package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.m
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
+import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.enums.AddressType
 import java.time.ZonedDateTime
 import java.util.*
 
@@ -19,17 +25,25 @@ data class CurfewReleaseDateConditions(
   @Column(name = "ORDER_ID", nullable = false, unique = true)
   val orderId: UUID,
 
+  @field:NotNull(message = "Enter curfew release day")
+  @field:Future(message = "Curfew release day must be in the future")
   @Column(name = "RELEASE_DATE", nullable = true)
   var releaseDate: ZonedDateTime? = null,
 
+  @field:NotNull(message = "Enter start time")
+  @field:Size(min = 1, message = "Enter start time")
   @Column(name = "START_TIME", nullable = true)
   var startTime: String? = null,
 
+  @field:NotNull(message = "Enter end time")
+  @field:Size(min = 1, message = "Enter end time")
   @Column(name = "END_TIME", nullable = true)
   var endTime: String? = null,
 
+  @Enumerated(EnumType.STRING)
+  @field:NotNull(message = "Curfew address is required")
   @Column(name = "CURFEW_ADDRESS", nullable = true)
-  var curfewAddress: UUID? = null,
+  var curfewAddress: AddressType? = null,
 
   @OneToOne
   @JoinColumn(name = "ORDER_ID", updatable = false, insertable = false)
