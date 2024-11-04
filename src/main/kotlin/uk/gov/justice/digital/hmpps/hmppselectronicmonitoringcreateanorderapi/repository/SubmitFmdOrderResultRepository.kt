e65ea4ca920730406d4f5f2c65139d5646b4b266
Bingjie Liu <Bingjie.Liu@justice.gov.uk>
2024-11-04 15:14:12 +0000

    Update src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/repository/SubmitFmdOrderResultRepository.kt
    Co-authored-by: Ben Carter <51202986+ben-cart3r@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/repository/SubmitFmdOrderResultRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/repository/SubmitFmdOrderResultRepository.kt
@@ -4,4 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.SubmitFmsOrderResult
 import java.util.*
 
-interface SubmitFmdOrderResultRepository : JpaRepository<SubmitFmsOrderResult, UUID>
+interface SubmitFmsOrderResultRepository : JpaRepository<SubmitFmsOrderResult, UUID>
