589d4580d8d38c4edc2e8ce3b947d993d109a3d7
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2024-11-15 13:49:43 +0000

    Add the ability to search for orders by firstName and lastName (#100)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/criteria/OrderSearchCriteria.kt
@@ -0,0 +1,6 @@
+package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.criteria
+
+data class OrderSearchCriteria(
+  val searchTerm: String = "",
+  val username: String,
+)
