2188221e1ba87b965dfc5d01433cd24ebad5c982
Ben Carter <ben.carter@justice.gov.uk>
2024-12-24 13:12:04 +0000

    Update the create-env script

---
+++ scripts/create-env.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+SECRETS=$(kubectl get secret serco-secret -o json -n hmpps-ems-cemo-ui-dev)
+
+# Auth
+echo "HMPPS_AUTH_URL=https://sign-in-dev.hmpps.service.justice.gov.uk/auth" > .env
+
+# Database
+echo "DB_SERVER=localhost:5432" >> .env
+echo "DB_NAME=postgres" >> .env
+echo "DB_USER=postgres" >> .env
+echo "DB_PASS=postgres" >> .env
+
+# Document Management API
+echo "DOCUMENT_MANAGEMENT_URL=http://localhost:8081/" >> .env
+
+# FMS Integration
+echo "SERCO_AUTH_URL=$(jq -r '.data.SERCO_AUTH_URL' <<< "${SECRETS}" | base64 -d)" >> .env
+echo "SERCO_CLIENT_ID=$(jq -r '.data.SERCO_CLIENT_ID' <<< "${SECRETS}" | base64 -d)" >> .env
+echo "SERCO_CLIENT_SECRET=$(jq -r '.data.SERCO_CLIENT_SECRET' <<< "${SECRETS}" | base64 -d)" >> .env
+echo "SERCO_USERNAME=$(jq -r '.data.SERCO_USERNAME' <<< "${SECRETS}" | base64 -d)" >> .env
+echo "SERCO_PASSWORD=$(jq -r '.data.SERCO_PASSWORD' <<< "${SECRETS}" | base64 -d)" >> .env
+echo "SERCO_URL=$(jq -r '.data.SERCO_URL' <<< "${SECRETS}" | base64 -d)" >> .env
+
+# Feature Flags
+echo "CP_PROCESSING_ENABLED=false" >> .env
+echo "CP_FMS_INTEGRATION_ENABLED=false" >> .env
+echo "CEMO_FMS_INTEGRATION_ENABLED=true" >> .env
