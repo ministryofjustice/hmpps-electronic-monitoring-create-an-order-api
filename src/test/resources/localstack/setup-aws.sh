3353aa6eb8ed297977989af14d0b1ac2bae44e4f
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2025-02-13 11:39:25 +0000

    update setup script to set AWS_REGION

--- src/test/resources/localstack/setup-aws.sh
+++ src/test/resources/localstack/setup-aws.sh
@@ -6,6 +6,9 @@ export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_REGION=eu-west-2
 export PAGER=
 
+aws configure set region $AWS_REGION
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 # Create the bucket
 aws s3 --endpoint-url=http://localhost:4566 --region eu-west-2 ls s3://testbucket || aws --endpoint-url=http://localhost:4566 --region=eu-west-2 s3 mb s3://testbucket
 
