c87aca639a9139c7d12669b289c099eb2293b359
Ben Carter <ben.carter@justice.gov.uk>
2025-03-19 14:57:21 +0000

    Update installation and risk tests. Add test for invalid risk categroy

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/integration/resource/InstallationAndRiskControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/integration/resource/InstallationAndRiskControllerTest.kt
@@ -3,12 +3,16 @@ package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.i
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.BodyInserters
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.data.ValidationErrors
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.dto.UpdateInstallationAndRiskDto
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.resource.validator.ValidationError
 import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
 import java.util.*
 
@@ -21,127 +25,186 @@ class InstallationAndRiskControllerTest : IntegrationTestBase() {
     repo.deleteAll()
   }
 
-  @Test
-  fun `Installation and Risk for an order created by a different user are not update-able`() {
-    val order = createOrder()
-    val result = webTestClient.put()
-      .uri("/api/orders/${order.id}/installation-and-risk")
-      .contentType(MediaType.APPLICATION_JSON)
-      .body(
-        BodyInserters.fromValue(
-          mockValidRequestBody(),
+  @Nested
+  @DisplayName("PUT /api/orders/{orderId}/installation-and-risk")
+  inner class UpdateInstallationAndRisk {
+    @Test
+    fun `it should return an error if the order was not created by the user`() {
+      val order = createOrder()
+      val result = webTestClient.put()
+        .uri("/api/orders/${order.id}/installation-and-risk")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(
+          BodyInserters.fromValue(
+            mockValidRequestBody(),
+          ),
+        )
+        .headers(setAuthorisation("AUTH_ADM_2"))
+        .exchange()
+        .expectStatus()
+        .isNotFound
+        .expectBodyList(ErrorResponse::class.java)
+        .returnResult()
+      val error = result.responseBody!!.first()
+      Assertions.assertThat(
+        error.developerMessage,
+      ).isEqualTo("An editable order with ${order.id} does not exist")
+    }
+
+    @Test
+    fun `it should return not found if the order does not exist`() {
+      webTestClient.put()
+        .uri("/api/orders/${UUID.randomUUID()}/installation-and-risk")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(
+          BodyInserters.fromValue(
+            mockValidRequestBody(),
+          ),
+        )
+        .headers(setAuthorisation("AUTH_ADM"))
+        .exchange()
+        .expectStatus()
+        .isNotFound
+    }
+
+    @Test
+    fun `it should return an error if the order is in a submitted state`() {
+      val order = createSubmittedOrder()
+      val result = webTestClient.put()
+        .uri("/api/orders/${order.id}/installation-and-risk")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(
+          BodyInserters.fromValue(
+            mockValidRequestBody(),
+          ),
+        )
+        .headers(setAuthorisation())
+        .exchange()
+        .expectStatus()
+        .isNotFound
+        .expectBodyList(ErrorResponse::class.java)
+        .returnResult()
+
+      val error = result.responseBody!!.first()
+      Assertions.assertThat(
+        error.developerMessage,
+      ).isEqualTo("An editable order with ${order.id} does not exist")
+    }
+
+    @Test
+    fun `it should update installation and risk with valid request body`() {
+      val order = createOrder()
+      val mockRisk = mockValidRequestBody(
+        offence = "MockOffence",
+        riskCategory = arrayOf("SEXUAL_OFFENCES"),
+        riskDetails = "mockRisk",
+        mappaLevel = "mockMappaLevel",
+        mappaCaseType = "mockMappaType",
+      )
+      webTestClient.put()
+        .uri("/api/orders/${order.id}/installation-and-risk")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(
+          BodyInserters.fromValue(
+            mockRisk,
+          ),
+        )
+        .headers(setAuthorisation())
+        .exchange()
+        .expectStatus()
+        .isOk
+
+      // Get updated order
+      val updatedOrder = getOrder(order.id)
+
+      Assertions.assertThat(updatedOrder.installationAndRisk?.offence).isEqualTo("MockOffence")
+      Assertions.assertThat(updatedOrder.installationAndRisk?.riskCategory?.first()).isEqualTo("SEXUAL_OFFENCES")
+      Assertions.assertThat(updatedOrder.installationAndRisk?.riskDetails).isEqualTo("mockRisk")
+      Assertions.assertThat(updatedOrder.installationAndRisk?.mappaLevel).isEqualTo("mockMappaLevel")
+      Assertions.assertThat(updatedOrder.installationAndRisk?.mappaCaseType).isEqualTo("mockMappaType")
+    }
+
+    @Test
+    fun `it should update installation and risk with default values`() {
+      val order = createOrder()
+      val mockRisk = mockValidRequestBody()
+
+      webTestClient.put()
+        .uri("/api/orders/${order.id}/installation-and-risk")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(
+          BodyInserters.fromValue(
+            mockRisk,
+          ),
+        )
+        .headers(setAuthorisation())
+        .exchange()
+        .expectStatus()
+        .isOk
+
+      // Get updated order
+      val updatedOrder = getOrder(order.id)
+
+      Assertions.assertThat(updatedOrder.installationAndRisk?.offence).isNull()
+      Assertions.assertThat(updatedOrder.installationAndRisk?.riskCategory).isNull()
+      Assertions.assertThat(updatedOrder.installationAndRisk?.riskDetails).isNull()
+      Assertions.assertThat(updatedOrder.installationAndRisk?.mappaLevel).isNull()
+      Assertions.assertThat(updatedOrder.installationAndRisk?.mappaCaseType).isNull()
+    }
+
+    @Test
+    fun `it should return an error if an invalid risk category is submitted`() {
+      val order = createOrder()
+
+      val result = webTestClient.put()
+        .uri("/api/orders/${order.id}/installation-and-risk")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(
+          BodyInserters.fromValue(
+            """
+              {
+                "offence": "",
+                "riskCategory": ["INVALID"],
+                "riskDetails": "",
+                "mappaLevel": "",
+                "mappaCaseType": ""
+              }
+            """.trimIndent(),
+          ),
+        )
+        .headers(setAuthorisation())
+        .exchange()
+        .expectStatus()
+        .isBadRequest
+        .expectBodyList(ValidationError::class.java)
+        .returnResult()
+        .responseBody
+
+      Assertions.assertThat(result).isNotNull
+      Assertions.assertThat(result).hasSize(1)
+      Assertions.assertThat(result).contains(
+        ValidationError(
+          "riskCategory",
+          ValidationErrors.InstallationAndRisk.RISK_CATEGORY_VALID,
         ),
       )
-      .headers(setAuthorisation("AUTH_ADM_2"))
-      .exchange()
-      .expectStatus()
-      .isNotFound
-      .expectBodyList(ErrorResponse::class.java)
-      .returnResult()
-    val error = result.responseBody!!.first()
-    Assertions.assertThat(
-      error.developerMessage,
-    ).isEqualTo("An editable order with ${order.id} does not exist")
-  }
+    }
 
-  @Test
-  fun `Installation and Risk for an order already submitted are not update-able`() {
-    val order = createSubmittedOrder()
-    val result = webTestClient.put()
-      .uri("/api/orders/${order.id}/installation-and-risk")
-      .contentType(MediaType.APPLICATION_JSON)
-      .body(
-        BodyInserters.fromValue(
-          mockValidRequestBody(),
-        ),
+    fun mockValidRequestBody(
+      offence: String? = null,
+      riskCategory: Array<String>? = null,
+      riskDetails: String? = null,
+      mappaLevel: String? = null,
+      mappaCaseType: String? = null,
+    ): String {
+      val condition = UpdateInstallationAndRiskDto(
+        offence = offence,
+        riskCategory = riskCategory,
+        riskDetails = riskDetails,
+        mappaLevel = mappaLevel,
+        mappaCaseType = mappaCaseType,
       )
-      .headers(setAuthorisation())
-      .exchange()
-      .expectStatus()
-      .isNotFound
-      .expectBodyList(ErrorResponse::class.java)
-      .returnResult()
-    val error = result.responseBody!!.first()
-    Assertions.assertThat(
-      error.developerMessage,
-    ).isEqualTo("An editable order with ${order.id} does not exist")
-  }
-
-  @Test
-  fun `Should save order with updated installation and risk`() {
-    val order = createOrder()
-    val mockRisk = mockValidRequestBody(
-      offence = "MockOffence",
-      riskCategory = arrayOf("MockCategory"),
-      riskDetails = "mockRisk",
-      mappaLevel = "mockMappaLevel",
-      mappaCaseType = "mockMappaType",
-    )
-    webTestClient.put()
-      .uri("/api/orders/${order.id}/installation-and-risk")
-      .contentType(MediaType.APPLICATION_JSON)
-      .body(
-        BodyInserters.fromValue(
-          mockRisk,
-        ),
-      )
-      .headers(setAuthorisation())
-      .exchange()
-      .expectStatus()
-      .isOk
-
-    // Get updated order
-    val updatedOrder = getOrder(order.id)
-
-    Assertions.assertThat(updatedOrder.installationAndRisk?.offence).isEqualTo("MockOffence")
-    Assertions.assertThat(updatedOrder.installationAndRisk?.riskCategory?.first()).isEqualTo("MockCategory")
-    Assertions.assertThat(updatedOrder.installationAndRisk?.riskDetails).isEqualTo("mockRisk")
-    Assertions.assertThat(updatedOrder.installationAndRisk?.mappaLevel).isEqualTo("mockMappaLevel")
-    Assertions.assertThat(updatedOrder.installationAndRisk?.mappaCaseType).isEqualTo("mockMappaType")
-  }
-
-  @Test
-  fun `Should save order with updated installation and risk will all default value`() {
-    val order = createOrder()
-    val mockRisk = mockValidRequestBody()
-
-    webTestClient.put()
-      .uri("/api/orders/${order.id}/installation-and-risk")
-      .contentType(MediaType.APPLICATION_JSON)
-      .body(
-        BodyInserters.fromValue(
-          mockRisk,
-        ),
-      )
-      .headers(setAuthorisation())
-      .exchange()
-      .expectStatus()
-      .isOk
-
-    // Get updated order
-    val updatedOrder = getOrder(order.id)
-
-    Assertions.assertThat(updatedOrder.installationAndRisk?.offence).isNull()
-    Assertions.assertThat(updatedOrder.installationAndRisk?.riskCategory).isNull()
-    Assertions.assertThat(updatedOrder.installationAndRisk?.riskDetails).isNull()
-    Assertions.assertThat(updatedOrder.installationAndRisk?.mappaLevel).isNull()
-    Assertions.assertThat(updatedOrder.installationAndRisk?.mappaCaseType).isNull()
-  }
-
-  fun mockValidRequestBody(
-    offence: String? = null,
-    riskCategory: Array<String>? = null,
-    riskDetails: String? = null,
-    mappaLevel: String? = null,
-    mappaCaseType: String? = null,
-  ): String {
-    val condition = UpdateInstallationAndRiskDto(
-      offence = offence,
-      riskCategory = riskCategory,
-      riskDetails = riskDetails,
-      mappaLevel = mappaLevel,
-      mappaCaseType = mappaCaseType,
-    )
-    return objectMapper.writeValueAsString(condition)
+      return objectMapper.writeValueAsString(condition)
+    }
   }
 }
