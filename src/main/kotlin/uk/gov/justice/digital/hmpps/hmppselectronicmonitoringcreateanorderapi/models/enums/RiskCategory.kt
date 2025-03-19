addf16d6bd5bbaee11ac21ae3827e358cbd48408
Ben Carter <ben.carter@justice.gov.uk>
2025-03-19 16:02:33 +0000

    Update risk category list to match DDv4.1

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/enums/RiskCategory.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/enums/RiskCategory.kt
@@ -13,6 +13,5 @@ enum class RiskCategory(val value: String) {
   OTHER_OCCUPANTS("Other occupants who pose a risk to staff"),
   OTHER_RISKS("Other known Risks"),
   HOMOPHOBIC_VIEWS("Is there evidence known to the subject having homophobic views"),
-  OFFENCE_RISK("Offence Risk"),
-  POSTCODE_RISK("Postcode Risk"),
+  UNDER_18("Under 18 living at property")
 }
