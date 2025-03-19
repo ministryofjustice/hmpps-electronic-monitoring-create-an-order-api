ebadb0a3a808c024209ceb66b369fb307e6a4b94
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2025-03-19 17:56:43 +0000

    Validate risk categories (#175)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/dto/UpdateInstallationAndRiskDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/dto/UpdateInstallationAndRiskDto.kt
@@ -1,5 +1,9 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.dto
 
+import jakarta.validation.constraints.AssertTrue
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.data.ValidationErrors
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.enums.RiskCategory
+
 data class UpdateInstallationAndRiskDto(
   val offence: String? = "",
 
@@ -10,4 +14,15 @@ data class UpdateInstallationAndRiskDto(
   val mappaLevel: String? = "",
 
   val mappaCaseType: String? = "",
-)
+) {
+  @AssertTrue(message = ValidationErrors.InstallationAndRisk.RISK_CATEGORY_VALID)
+  fun isRiskCategory(): Boolean {
+    if (riskCategory == null) {
+      return true
+    }
+
+    return riskCategory.all {
+      RiskCategory.entries.any { riskCategory -> riskCategory.name == it }
+    }
+  }
+}
