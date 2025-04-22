39bd243e0aae72c61beb72c36c496d68e473cc41
Ben Carter <ben.carter@justice.gov.uk>
2025-04-22 13:45:51 +0100

    Add failing test for mapping gender to Serco from saved values

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/model/fms/DeviceWearerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/model/fms/DeviceWearerTest.kt
@@ -33,6 +33,17 @@ class DeviceWearerTest {
     assertThat(fmsDeviceWearer.sex).isEqualTo(mappedValue)
   }
 
+  @ParameterizedTest(name = "it should map saved gender values to Serco - {0} -> {1}")
+  @MethodSource("genderValues")
+  fun `It should map correctly map saved gender values to Serco`(savedValue: String, mappedValue: String) {
+    val order = createOrder(
+      deviceWearer = createDeviceWearer(gender = savedValue),
+    )
+    val fmsDeviceWearer = FmsDeviceWearer.fromCemoOrder(order)
+
+    assertThat(fmsDeviceWearer.genderIdentity).isEqualTo(mappedValue)
+  }
+
   companion object {
     @JvmStatic
     fun sexValues() = listOf(
@@ -40,6 +51,15 @@ class DeviceWearerTest {
       Arguments.of("FEMALE", "Female"),
       Arguments.of("PREFER_NOT_TO_SAY", "Prefer Not to Say"),
       Arguments.of("UNKNOWN", "Prefer Not to Say"),
+    )
+
+    @JvmStatic
+    fun genderValues() = listOf(
+      Arguments.of("MALE", "Male"),
+      Arguments.of("FEMALE", "Female"),
+      Arguments.of("NON_BINARY", "Non-Binary"),
+      Arguments.of("PREFER_TO_SELF_DESCRIBE", "Prefer to self-describe"),
+      Arguments.of("NOT_ABLE_TO_PROVIDE_THIS_INFORMATION", ""),
     )
   }
 
