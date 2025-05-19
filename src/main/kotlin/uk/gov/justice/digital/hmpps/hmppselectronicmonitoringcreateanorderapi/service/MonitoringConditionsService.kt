c949819f9e0b58b9e7f0a125d79794a345c2fa02
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2025-05-19 13:56:26 +0100

    Refactor to default CEMO monitoring conditions start time to 00:00 and end time to 23:59.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/service/MonitoringConditionsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/service/MonitoringConditionsService.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.s
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.MonitoringConditions
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.dto.UpdateMonitoringConditionsDto
+import java.time.ZonedDateTime
 import java.util.*
 
 @Service
@@ -18,8 +19,16 @@ class MonitoringConditionsService : OrderSectionServiceBase() {
     order.monitoringConditions = MonitoringConditions(
       versionId = order.getCurrentVersion().id,
       orderType = updateRecord.orderType,
-      startDate = updateRecord.startDate,
-      endDate = updateRecord.endDate,
+      startDate = ZonedDateTime.of(
+        updateRecord.startDate!!.year,
+        updateRecord.startDate.monthValue,
+        updateRecord.startDate.dayOfMonth,
+        0,
+        0,
+        0,
+        0,
+        updateRecord.startDate.zone,
+      ),
       orderTypeDescription = updateRecord.orderTypeDescription,
       conditionType = updateRecord.conditionType,
       curfew = updateRecord.curfew,
@@ -32,6 +41,18 @@ class MonitoringConditionsService : OrderSectionServiceBase() {
       hdc = updateRecord.hdc,
       prarr = updateRecord.prarr,
     )
+    if (updateRecord.endDate != null) {
+      order.monitoringConditions!!.endDate = ZonedDateTime.of(
+        updateRecord.endDate.year,
+        updateRecord.endDate.monthValue,
+        updateRecord.endDate.dayOfMonth,
+        23,
+        59,
+        0,
+        0,
+        updateRecord.startDate.zone,
+      )
+    }
 
     return orderRepo.save(order).monitoringConditions!!
   }
