921ea61e3a05acfe67f99ebffe2b2db462d1b1e6
James Francis <james.francis@madetech.com>
2025-07-24 14:39:41 +0100

    cemo 5 and 7

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/integration/scenarios/ScenarioTests.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/integration/scenarios/ScenarioTests.kt
@@ -43,7 +43,35 @@ class ScenarioTests : IntegrationTestBase() {
   companion object {
     @JvmStatic
     fun scenarios() = listOf(
+//      Arguments.of("cemo001", "src/test/resources/json/scenarios/cemo001"),
       Arguments.of("cemo002", "src/test/resources/json/scenarios/cemo002"),
+//      Arguments.of("cemo003", "src/test/resources/json/scenarios/cemo003"),
+//      Arguments.of("cemo004", "src/test/resources/json/scenarios/cemo004"),
+      Arguments.of("cemo005", "src/test/resources/json/scenarios/cemo005"),
+//      Arguments.of("cemo006", "src/test/resources/json/scenarios/cemo006"),
+      Arguments.of("cemo007", "src/test/resources/json/scenarios/cemo007"),
+//      Arguments.of("cemo008", "src/test/resources/json/scenarios/cemo008"),
+//      Arguments.of("cemo009", "src/test/resources/json/scenarios/cemo009"),
+//      Arguments.of("cemo010", "src/test/resources/json/scenarios/cemo010"),
+//      Arguments.of("cemo011", "src/test/resources/json/scenarios/cemo011"),
+//      Arguments.of("cemo012", "src/test/resources/json/scenarios/cemo012"),
+//      Arguments.of("cemo013", "src/test/resources/json/scenarios/cemo013"),
+//      Arguments.of("cemo014", "src/test/resources/json/scenarios/cemo014"),
+//      Arguments.of("cemo015", "src/test/resources/json/scenarios/cemo015"),
+//      Arguments.of("cemo016", "src/test/resources/json/scenarios/cemo016"),
+//      Arguments.of("cemo017", "src/test/resources/json/scenarios/cemo017"),
+//      Arguments.of("cemo018", "src/test/resources/json/scenarios/cemo018"),
+//      Arguments.of("cemo019", "src/test/resources/json/scenarios/cemo019"),
+//      Arguments.of("cemo020", "src/test/resources/json/scenarios/cemo020"),
+//      Arguments.of("cemo021", "src/test/resources/json/scenarios/cemo021"),
+//      Arguments.of("cemo022", "src/test/resources/json/scenarios/cemo022"),
+//      Arguments.of("cemo023", "src/test/resources/json/scenarios/cemo023"),
+//      Arguments.of("cemo024", "src/test/resources/json/scenarios/cemo024"),
+//      Arguments.of("cemo025", "src/test/resources/json/scenarios/cemo025"),
+//      Arguments.of("cemo026", "src/test/resources/json/scenarios/cemo026"),
+//      Arguments.of("cemo027", "src/test/resources/json/scenarios/cemo027"),
+//      Arguments.of("cemo028", "src/test/resources/json/scenarios/cemo028"),
+//      Arguments.of("cemo029", "src/test/resources/json/scenarios/cemo029"),
     )
   }
 
@@ -66,18 +94,20 @@ class ScenarioTests : IntegrationTestBase() {
 
     repo.save(order)
 
-    sercoApi.stubSubmitAttachment(
-      HttpStatus.OK,
-      FmsAttachmentResponse(
-        result = FmsAttachmentResult(
-          fileName = order.additionalDocuments[0].fileName!!,
-          tableName = "x_serg2_ems_csm_sr_mo_new",
-          sysId = "MockSysId",
-          tableSysId = "MockDeviceWearerId",
+    if (order.additionalDocuments.isNotEmpty()) {
+      sercoApi.stubSubmitAttachment(
+        HttpStatus.OK,
+        FmsAttachmentResponse(
+          result = FmsAttachmentResult(
+            fileName = order.additionalDocuments[0].fileName!!,
+            tableName = "x_serg2_ems_csm_sr_mo_new",
+            sysId = "MockSysId",
+            tableSysId = "MockDeviceWearerId",
+          ),
         ),
-      ),
-    )
-    documentApi.stubGetDocument(order.additionalDocuments.first().id.toString())
+      )
+      documentApi.stubGetDocument(order.additionalDocuments.first().id.toString())
+    }
 
     if (order.enforcementZoneConditions.isNotEmpty()) {
       sercoApi.stubSubmitAttachment(
