5657f015a9b2e73a09b53c13d50e725b3e897be0
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2025-07-30 12:17:37 +0100

    Corrections to address validation error messages. Validate device wearer's addresses for max number of characters.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/dto/UpdateAddressDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/dto/UpdateAddressDto.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.dto
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.data.ValidationErrors
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.enums.AddressType
 
@@ -7,15 +8,20 @@ data class UpdateAddressDto(
   val addressType: AddressType,
 
   @field:NotBlank(message = ValidationErrors.Address.ADDRESS_1_REQUIRED)
+  @field:Size(max = 200, message = ValidationErrors.Address.ADDRESS_1_MAX_LENGTH)
   val addressLine1: String = "",
 
+  @field:Size(max = 200, message = ValidationErrors.Address.ADDRESS_2_MAX_LENGTH)
   val addressLine2: String = "",
 
   @field:NotBlank(message = ValidationErrors.Address.ADDRESS_3_REQUIRED)
+  @field:Size(max = 200, message = ValidationErrors.Address.ADDRESS_3_MAX_LENGTH)
   val addressLine3: String = "",
 
+  @field:Size(max = 200, message = ValidationErrors.Address.ADDRESS_4_MAX_LENGTH)
   val addressLine4: String = "",
 
   @field:NotBlank(message = ValidationErrors.Address.POSTCODE_REQUIRED)
+  @field:Size(max = 200, message = ValidationErrors.Address.POSTCODE_MAX_LENGTH)
   val postcode: String = "",
 )
