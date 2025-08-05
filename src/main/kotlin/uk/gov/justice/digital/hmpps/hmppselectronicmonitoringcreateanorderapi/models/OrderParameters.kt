ded5a4a408290166630638f189e33182d650e083
James Francis <james.francis@madetech.com>
2025-08-05 16:23:13 +0100

    create order parameters table

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/OrderParameters.kt
@@ -0,0 +1,21 @@
+package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.util.*
+
+@Entity
+@Table(name = "ORDER_PARAMETERS")
+data class OrderParameters(
+  @Id
+  @Column(name = "ID", nullable = false, unique = true)
+  val id: UUID = UUID.randomUUID(),
+
+  @Column(name = "VERSION_ID", nullable = false)
+  val versionId: UUID,
+
+  @Column(name = "HAVE_PHOTO", nullable = false)
+  val havePhoto: Boolean,
+)
