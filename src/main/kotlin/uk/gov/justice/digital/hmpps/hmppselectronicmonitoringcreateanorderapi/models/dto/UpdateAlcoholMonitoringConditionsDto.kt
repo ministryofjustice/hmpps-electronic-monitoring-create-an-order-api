48f93ec66a5760d55f41b92a2650470032a83e4f
James Francis <70900829+james-francis-MT@users.noreply.github.com>
2025-08-15 13:38:30 +0100

    remove validation on installation address (#269)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/dto/UpdateAlcoholMonitoringConditionsDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/dto/UpdateAlcoholMonitoringConditionsDto.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.dto
 
-import jakarta.validation.constraints.AssertTrue
 import jakarta.validation.constraints.Future
 import jakarta.validation.constraints.NotNull
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.data.ValidationErrors
@@ -18,22 +17,9 @@ data class UpdateAlcoholMonitoringConditionsDto(
   @field:Future(message = ValidationErrors.AlcoholMonitoring.END_DATE_MUST_BE_IN_FUTURE)
   val endDate: ZonedDateTime? = null,
 
-  @field:NotNull(message = ValidationErrors.AlcoholMonitoring.INSTALLATION_LOCATION_REQUIRED)
   val installationLocation: InstallationLocationType? = null,
 
   val prisonName: String? = null,
-  val probationOfficeName: String? = null,
-) {
-  @AssertTrue(message = ValidationErrors.AlcoholMonitoring.PRISON_NAME_REQUIRED_LOCATION_IS_PRISON)
-  fun isPrisonName(): Boolean = !(
-    installationLocation == InstallationLocationType.PRISON && prisonName.isNullOrBlank()
-    )
 
-  @AssertTrue(
-    message = ValidationErrors.AlcoholMonitoring.OFFICE_NAME_REQUIRED_LOCATION_IS_PROBATION_OFFICE,
-  )
-  fun isProbationOfficeName(): Boolean = !(
-    installationLocation == InstallationLocationType.PROBATION_OFFICE &&
-      probationOfficeName.isNullOrBlank()
-    )
-}
+  val probationOfficeName: String? = null,
+)
