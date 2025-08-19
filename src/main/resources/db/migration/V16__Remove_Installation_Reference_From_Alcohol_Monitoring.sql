4dfaad21fb483617ec61c900cb38ef025debf3e6
James Francis <james.francis@madetech.com>
2025-08-19 13:14:11 +0100

    fix migration

--- src/main/resources/db/migration/V16__Remove_Installation_Reference_From_Alcohol_Monitoring.sql
+++ src/main/resources/db/migration/V16__Remove_Installation_Reference_From_Alcohol_Monitoring.sql
@@ -1,20 +1,7 @@
 -- https://dsdmoj.atlassian.net/browse/ELM-4016
 ALTER TABLE alcohol_monitoring
-DROP
-CONSTRAINT fk_alcohol_monitoring_on_installation_address;
-
-ALTER TABLE alcohol_monitoring
-DROP
-COLUMN installation_address_id;
-
-ALTER TABLE alcohol_monitoring
-DROP
-COLUMN installation_location;
-
-ALTER TABLE alcohol_monitoring
-DROP
-COLUMN prison_name;
-
-ALTER TABLE alcohol_monitoring
-DROP
-COLUMN probation_office_name;
+    DROP CONSTRAINT IF EXISTS fk_alcohol_monitoring_on_installation_address,
+    DROP COLUMN IF EXISTS installation_address_id,
+    DROP COLUMN IF EXISTS installation_location,
+    DROP COLUMN IF EXISTS prison_name,
+    DROP COLUMN IF EXISTS probation_office_name;
