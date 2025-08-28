d61cb094ad76f580b369b83d78ab09721a3f8a99
Dave Arrowsmith <devarrowsmith@gmail.com>
2025-08-28 14:34:45 +0100

    DDv5-specific Notifying Organisation validation (#281)
    * If order version is not DDv4, make notifying organisation name mandatory for probation and family court orders.
    
    * Update a hardcoded date in scenario tests.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/service/InterestedPartiesService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/service/InterestedPartiesService.kt
@@ -1,8 +1,14 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.service
 
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.data.ValidationErrors
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.exception.FormValidationException
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.InterestedParties
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.dto.UpdateInterestedPartiesDto
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.enums.DataDictionaryVersion
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.enums.FamilyCourtDDv5
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.enums.NotifyingOrganisationDDv5
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.enums.ProbationServiceRegion
 import java.util.*
 
 @Service
@@ -12,8 +18,25 @@ class InterestedPartiesService(private val addressService: AddressService) : Ord
     username: String,
     updateRecord: UpdateInterestedPartiesDto,
   ): InterestedParties {
-    // Verify the order belongs to the user and is in draft state
     val order = this.findEditableOrder(orderId, username)
+
+    if (order.dataDictionaryVersion != DataDictionaryVersion.DDV4) {
+      val invalidNotifyingOrganisation =
+        when (updateRecord.notifyingOrganisation) {
+          NotifyingOrganisationDDv5.FAMILY_COURT ->
+            FamilyCourtDDv5.entries.none { it.name == updateRecord.notifyingOrganisationName }
+          NotifyingOrganisationDDv5.PROBATION ->
+            ProbationServiceRegion.entries.none { it.name == updateRecord.notifyingOrganisationName }
+          else -> false
+        }
+
+      if (invalidNotifyingOrganisation) {
+        throw FormValidationException(
+          "notifyingOrganisationName",
+          ValidationErrors.InterestedParties.NOTIFYING_ORGANISATION_NAME_REQUIRED,
+        )
+      }
+    }
 
     val newInterestedParties = InterestedParties(
       versionId = order.getCurrentVersion().id,
@@ -26,13 +49,15 @@ class InterestedPartiesService(private val addressService: AddressService) : Ord
       responsibleOrganisationRegion = updateRecord.responsibleOrganisationRegion,
       responsibleOrganisationEmail = updateRecord.responsibleOrganisationEmail,
     )
-    // clear previously selected probation delivery unit if responsible organisation or responsible organisation region changed
+
     if (newInterestedParties.responsibleOrganisation != order.interestedParties?.responsibleOfficerName ||
       newInterestedParties.responsibleOrganisationRegion != order.interestedParties?.responsibleOrganisationRegion
     ) {
       order.probationDeliveryUnit = null
     }
+
     order.interestedParties = newInterestedParties
+
     return orderRepo.save(order).interestedParties!!
   }
 }
