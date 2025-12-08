d5738664e2b864a74870943594e1b0c71f44a35c
Bingjie Liu <Bingjie.Liu@justice.gov.uk>
2025-12-08 12:32:04 +0000

    Map subcategory to FMS (#350)
    Co-authored-by: Bingjie Liu <bingjie.liu@digital.justice.gov.uk>

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/model/fms/argumentsProvider/OrderTypeArgumentsProvider.kt
@@ -0,0 +1,23 @@
+package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.model.fms.argumentsProvider
+
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.enums.RequestType
+import java.util.stream.Stream
+
+class OrderTypeArgumentsProvider : ArgumentsProvider {
+  override fun provideArguments(context: ExtensionContext?): Stream<out Arguments?>? = Stream.of(
+    Arguments.of(RequestType.REQUEST, ""),
+    Arguments.of(RequestType.REQUEST, ""),
+    Arguments.of(RequestType.VARIATION, "SR08 - Amend monitoring requirements"),
+    Arguments.of(RequestType.REJECTED, ""),
+    Arguments.of(RequestType.AMEND_ORIGINAL_REQUEST, ""),
+    Arguments.of(
+      RequestType.REINSTALL_AT_DIFFERENT_ADDRESS,
+      "SR05 - Install monitoring equipment at an additional address",
+    ),
+    Arguments.of(RequestType.REINSTALL_DEVICE, "SR04 - Re-install monitoring equipment"),
+    Arguments.of(RequestType.REVOCATION, "SR21 - Revocation monitoring requirements"),
+  )
+}
