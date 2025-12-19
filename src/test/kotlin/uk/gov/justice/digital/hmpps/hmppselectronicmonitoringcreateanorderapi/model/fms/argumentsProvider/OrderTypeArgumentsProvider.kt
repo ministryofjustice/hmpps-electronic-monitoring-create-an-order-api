cef1a8490f4f2759dcca4eadecee39b5807df23f
James Francis <james.francis@madetech.com>
2025-12-19 15:34:56 +0000

    update SR codes

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/model/fms/argumentsProvider/OrderTypeArgumentsProvider.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/model/fms/argumentsProvider/OrderTypeArgumentsProvider.kt
@@ -10,14 +10,14 @@ class OrderTypeArgumentsProvider : ArgumentsProvider {
   override fun provideArguments(context: ExtensionContext?): Stream<out Arguments?>? = Stream.of(
     Arguments.of(RequestType.REQUEST, ""),
     Arguments.of(RequestType.REQUEST, ""),
-    Arguments.of(RequestType.VARIATION, "SR08 - Amend monitoring requirements"),
+    Arguments.of(RequestType.VARIATION, "SR08-Amend monitoring requirements"),
     Arguments.of(RequestType.REJECTED, ""),
     Arguments.of(RequestType.AMEND_ORIGINAL_REQUEST, ""),
     Arguments.of(
       RequestType.REINSTALL_AT_DIFFERENT_ADDRESS,
-      "SR05 - Install monitoring equipment at an additional address",
+      "SR05-Install monitoring equipment at an additional address",
     ),
-    Arguments.of(RequestType.REINSTALL_DEVICE, "SR04 - Re-install monitoring equipment"),
-    Arguments.of(RequestType.REVOCATION, "SR21 - Revocation monitoring requirements"),
+    Arguments.of(RequestType.REINSTALL_DEVICE, "SR04-Re-install monitoring equipment"),
+    Arguments.of(RequestType.REVOCATION, "SR21-Revocation monitoring requirements"),
   )
 }
