e3b036534674b102884057ee2322b19a2a52295b
James Francis <james.francis@madetech.com>
2026-01-15 11:44:46 +0000

    add extra int tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/integration/resource/DapoControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/integration/resource/DapoControllerTest.kt
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.web.reactive.function.BodyInserters
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.dto.UpdateDapoDto
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
 import java.time.ZoneId
 import java.time.ZonedDateTime
+import java.util.UUID
 
 class DapoControllerTest : IntegrationTestBase() {
 
@@ -27,16 +30,7 @@ class DapoControllerTest : IntegrationTestBase() {
     val order = createOrder()
     val mockDate = ZonedDateTime.of(2025, 2, 2, 0, 0, 0, 0, ZoneId.of("UTC"))
 
-    webTestClient.put()
-      .uri("/api/orders/${order.id}/dapo")
-      .contentType(MediaType.APPLICATION_JSON)
-      .body(
-        BodyInserters.fromValue(
-          mockValidRequestBody(clause = "some clause", date = mockDate),
-        ),
-      )
-      .headers(setAuthorisation())
-      .exchange()
+    callDapoEndpoint(order.id, mockValidRequestBody(clause = "some clause", date = mockDate))
       .expectStatus()
       .isOk
 
@@ -48,8 +42,97 @@ class DapoControllerTest : IntegrationTestBase() {
     Assertions.assertThat(dapoClause.date).isEqualTo(mockDate)
   }
 
-  private fun mockValidRequestBody(clause: String? = null, date: ZonedDateTime? = null): String {
-    val dto = UpdateDapoDto(clause = clause, date = date)
+  @Test
+  fun `should update a dapo clause`() {
+    val order = createOrder()
+    val mockDate = ZonedDateTime.of(2025, 2, 2, 0, 0, 0, 0, ZoneId.of("UTC"))
+
+    callDapoEndpoint(order.id, mockValidRequestBody(clause = "some clause", date = mockDate))
+      .expectStatus()
+      .isOk
+
+    val orderWithDapo = getOrder(order.id)
+    val dapoId = orderWithDapo.dapoClauses.first().id
+
+    callDapoEndpoint(order.id, mockValidRequestBody(id = dapoId, clause = "some clause", date = mockDate))
+      .expectStatus()
+      .isOk
+
+    val orderWithUpdatedDapo = getOrder(order.id)
+
+    Assertions.assertThat(orderWithUpdatedDapo.dapoClauses.size).isEqualTo(1)
+    val dapoClause = orderWithUpdatedDapo.dapoClauses.first()
+    Assertions.assertThat(dapoClause.id).isEqualTo(dapoId)
+    Assertions.assertThat(dapoClause.clause).isEqualTo("some clause")
+    Assertions.assertThat(dapoClause.date).isEqualTo(mockDate)
+  }
+
+  @Test
+  fun `it should return an error if the order was not created by the user`() {
+    val order = createOrder()
+
+    val result =
+      callDapoEndpoint(
+        order.id,
+        mockValidRequestBody(clause = "some clause", date = ZonedDateTime.now()),
+        username = "AUTH_ADM_2",
+      )
+        .expectStatus()
+        .isNotFound
+        .expectBodyList(ErrorResponse::class.java)
+        .returnResult()
+
+    val error = result.responseBody!!.first()
+    Assertions.assertThat(
+      error.developerMessage,
+    ).isEqualTo("An editable order with ${order.id} does not exist")
+  }
+
+  @Test
+  fun `it should return not found if the order does not exist`() {
+    callDapoEndpoint(UUID.randomUUID(), mockValidRequestBody(clause = "some clause", date = ZonedDateTime.now()))
+      .expectStatus()
+      .isNotFound
+  }
+
+  @Test
+  fun `it should return an error if the order is in a submitted state`() {
+    val order = createSubmittedOrder()
+    val result = callDapoEndpoint(
+      order.id,
+      mockValidRequestBody(clause = "some clause", date = ZonedDateTime.now()),
+    )
+      .expectStatus()
+      .isNotFound
+      .expectBodyList(ErrorResponse::class.java)
+      .returnResult()
+
+    val error = result.responseBody!!.first()
+    Assertions.assertThat(
+      error.developerMessage,
+    ).isEqualTo("An editable order with ${order.id} does not exist")
+  }
+
+  private fun callDapoEndpoint(orderId: UUID, body: String, username: String? = null): WebTestClient.ResponseSpec {
+    val headers = if (username != null) setAuthorisation(username) else setAuthorisation()
+    return webTestClient.put()
+      .uri("/api/orders/$orderId/dapo")
+      .contentType(MediaType.APPLICATION_JSON)
+      .body(
+        BodyInserters.fromValue(
+          body,
+        ),
+      )
+      .headers(headers)
+      .exchange()
+  }
+
+  private fun mockValidRequestBody(
+    id: UUID? = UUID.randomUUID(),
+    clause: String? = null,
+    date: ZonedDateTime? = null,
+  ): String {
+    val dto = UpdateDapoDto(id = id, clause = clause, date = date)
 
     return objectMapper.writeValueAsString(dto)
   }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/integration/resource/OffenceControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/integration/resource/OffenceControllerTest.kt
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.web.reactive.function.BodyInserters
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.dto.UpdateOffenceDto
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
 import java.time.ZoneId
 import java.time.ZonedDateTime
+import java.util.*
 
 class OffenceControllerTest : IntegrationTestBase() {
 
@@ -27,16 +30,7 @@ class OffenceControllerTest : IntegrationTestBase() {
     val order = createOrder()
     val mockDate = ZonedDateTime.of(2025, 2, 2, 0, 0, 0, 0, ZoneId.of("UTC"))
 
-    webTestClient.put()
-      .uri("/api/orders/${order.id}/offence")
-      .contentType(MediaType.APPLICATION_JSON)
-      .body(
-        BodyInserters.fromValue(
-          mockValidRequestBody(offenceType = "some offence", offenceDate = mockDate),
-        ),
-      )
-      .headers(setAuthorisation())
-      .exchange()
+    callOffenceEndpoint(order.id, mockValidRequestBody(offenceType = "some offence", offenceDate = mockDate))
       .expectStatus()
       .isOk
 
@@ -48,8 +42,101 @@ class OffenceControllerTest : IntegrationTestBase() {
     Assertions.assertThat(offence.offenceDate).isEqualTo(mockDate)
   }
 
-  private fun mockValidRequestBody(offenceType: String? = null, offenceDate: ZonedDateTime? = null): String {
-    val dto = UpdateOffenceDto(offenceType = offenceType, offenceDate = offenceDate)
+  @Test
+  fun `should update an offence`() {
+    val order = createOrder()
+    val mockDate = ZonedDateTime.of(2025, 2, 2, 0, 0, 0, 0, ZoneId.of("UTC"))
+
+    callOffenceEndpoint(order.id, mockValidRequestBody(offenceType = "some offence", offenceDate = mockDate))
+      .expectStatus()
+      .isOk
+
+    val orderWithOffence = getOrder(order.id)
+    val dapoId = orderWithOffence.offences.first().id
+
+    callOffenceEndpoint(
+      order.id,
+      mockValidRequestBody(id = dapoId, offenceType = "some offence", offenceDate = mockDate),
+    )
+      .expectStatus()
+      .isOk
+
+    val orderWithUpdatedOffence = getOrder(order.id)
+
+    Assertions.assertThat(orderWithUpdatedOffence.offences.size).isEqualTo(1)
+    val offence = orderWithUpdatedOffence.offences.first()
+    Assertions.assertThat(offence.id).isEqualTo(dapoId)
+    Assertions.assertThat(offence.offenceType).isEqualTo("some offence")
+    Assertions.assertThat(offence.offenceDate).isEqualTo(mockDate)
+  }
+
+  @Test
+  fun `it should return an error if the order was not created by the user`() {
+    val order = createOrder()
+
+    val result =
+      callOffenceEndpoint(
+        order.id,
+        mockValidRequestBody(offenceType = "some offence", offenceDate = ZonedDateTime.now()),
+        username = "AUTH_ADM_2",
+      )
+        .expectStatus()
+        .isNotFound
+        .expectBodyList(ErrorResponse::class.java)
+        .returnResult()
+
+    val error = result.responseBody!!.first()
+    Assertions.assertThat(
+      error.developerMessage,
+    ).isEqualTo("An editable order with ${order.id} does not exist")
+  }
+
+  @Test
+  fun `it should return not found if the order does not exist`() {
+    callOffenceEndpoint(UUID.randomUUID(), mockValidRequestBody())
+      .expectStatus()
+      .isNotFound
+  }
+
+  @Test
+  fun `it should return an error if the order is in a submitted state`() {
+    val order = createSubmittedOrder()
+
+    val result = callOffenceEndpoint(
+      order.id,
+      mockValidRequestBody(),
+    )
+      .expectStatus()
+      .isNotFound
+      .expectBodyList(ErrorResponse::class.java)
+      .returnResult()
+
+    val error = result.responseBody!!.first()
+    Assertions.assertThat(
+      error.developerMessage,
+    ).isEqualTo("An editable order with ${order.id} does not exist")
+  }
+
+  private fun callOffenceEndpoint(orderId: UUID, body: String, username: String? = null): WebTestClient.ResponseSpec {
+    val headers = if (username != null) setAuthorisation(username) else setAuthorisation()
+    return webTestClient.put()
+      .uri("/api/orders/$orderId/offence")
+      .contentType(MediaType.APPLICATION_JSON)
+      .body(
+        BodyInserters.fromValue(
+          body,
+        ),
+      )
+      .headers(headers)
+      .exchange()
+  }
+
+  private fun mockValidRequestBody(
+    id: UUID = UUID.randomUUID(),
+    offenceType: String? = null,
+    offenceDate: ZonedDateTime? = null,
+  ): String {
+    val dto = UpdateOffenceDto(id = id, offenceType = offenceType, offenceDate = offenceDate)
 
     return objectMapper.writeValueAsString(dto)
   }
