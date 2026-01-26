797ea9719040aa4296f2569acf19b1995ed3d71b
James Francis <james.francis@madetech.com>
2026-01-26 12:23:07 +0000

    create mappa controller

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/service/MappaService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/service/MappaService.kt
@@ -1,9 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.service
 
+import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.Mappa
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.dto.UpdateMappaDto
 import java.util.*
 
+@Service
 class MappaService : OrderSectionServiceBase() {
   fun updateMappa(mockOrderId: UUID, mockUsername: String, mockUpdateDto: UpdateMappaDto): Mappa {
     val order = this.findEditableOrder(mockOrderId, mockUsername)
