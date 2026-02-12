628a53847a99406ced979d343d993dc58a89b1fb
James Francis <james.francis@madetech.com>
2026-02-12 14:16:34 +0000

    add controller for delete

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/resource/DapoController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/resource/DapoController.kt
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -34,5 +35,16 @@ class DapoController(val service: DapoService) {
     )
 
     return ResponseEntity(dapo, HttpStatus.OK)
+  }
+
+  @DeleteMapping("/orders/{orderId}/dapo/delete/{dapoId}")
+  fun deleteDapo(
+    @PathVariable orderId: UUID,
+    @PathVariable dapoId: UUID,
+    authentication: Authentication,
+  ): ResponseEntity<Unit> {
+    val username = authentication.name
+    service.deleteDapo(orderId, username, dapoId)
+    return ResponseEntity(HttpStatus.NO_CONTENT)
   }
 }
