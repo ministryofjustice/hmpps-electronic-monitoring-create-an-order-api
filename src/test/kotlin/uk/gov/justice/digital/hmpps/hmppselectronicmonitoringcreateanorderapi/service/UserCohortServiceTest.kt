75c122c4b6898469bf6948ccca45719dbfde1ee0
James Francis <james.francis@madetech.com>
2026-02-13 13:16:40 +0000

    service gets cohort from user group

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/service/UserCohortServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/service/UserCohortServiceTest.kt
@@ -11,22 +11,23 @@ import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.test.context.ActiveProfiles
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.client.ManageUserApi
-import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.auth.Cohorts
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.auth.Cohort
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.auth.UserGroup
 
 @ActiveProfiles("test")
 @JsonTest
 class UserCohortServiceTest {
 
   private lateinit var authentication: JwtAuthenticationToken
-  private lateinit var jwtToken: Jwt
+  private lateinit var mockJwtToken: Jwt
   private lateinit var mockApi: ManageUserApi
   private lateinit var service: UserCohortService
 
   @BeforeEach
   fun setup() {
     authentication = mock(JwtAuthenticationToken::class.java)
-    jwtToken = mock(Jwt::class.java)
-    `when`(authentication.token).thenReturn(jwtToken)
+    mockJwtToken = mock(Jwt::class.java)
+    `when`(authentication.token).thenReturn(mockJwtToken)
 
     mockApi = mock()
     service = UserCohortService(mockApi)
@@ -35,13 +36,13 @@ class UserCohortServiceTest {
   @Test
   fun `Should return user cohort of prison when user has role ROLE_PRISON`() {
     `when`(authentication.authorities).thenReturn(listOf(GrantedAuthority { "ROLE_PRISON" }))
-    `when`(jwtToken.tokenValue).thenReturn("Mock Token")
-    `when`(mockApi.getUserActiveCaseload(authentication.token)).thenReturn(
+    `when`(mockJwtToken.tokenValue).thenReturn("Mock Token")
+    `when`(mockApi.getUserActiveCaseloadName(authentication.token)).thenReturn(
       "HMP ABC",
     )
     val result = service.getUserCohort(authentication)
 
-    Assertions.assertThat(result.cohort).isEqualTo(Cohorts.PRISON)
+    Assertions.assertThat(result.cohort).isEqualTo(Cohort.PRISON)
     Assertions.assertThat(result.activeCaseLoad).isEqualTo("HMP ABC")
   }
 
@@ -51,6 +52,30 @@ class UserCohortServiceTest {
 
     val result = service.getUserCohort(authentication)
 
-    Assertions.assertThat(result.cohort).isEqualTo(Cohorts.PROBATION)
+    Assertions.assertThat(result.cohort).isEqualTo(Cohort.PROBATION)
+  }
+
+  @Test
+  fun `returns court when user has court groups`() {
+    `when`(authentication.authorities).thenReturn(listOf(GrantedAuthority { "ROLE_OTHER" }))
+    `when`(mockApi.getUserGroups(mockJwtToken)).thenReturn(
+      listOf(UserGroup(groupName = "Create an Electronic Monitoring Order Court Users", groupCode = "CEMO_CRT_USERS")),
+    )
+
+    val result = service.getUserCohort(authentication)
+
+    Assertions.assertThat(result.cohort).isEqualTo(Cohort.COURT)
+  }
+
+  @Test
+  fun `returns home office when user has HO groups`() {
+    `when`(authentication.authorities).thenReturn(listOf(GrantedAuthority { "ROLE_OTHER" }))
+    `when`(mockApi.getUserGroups(mockJwtToken)).thenReturn(
+      listOf(UserGroup(groupName = "Create an Electronic Monitoring Order HO Users", groupCode = "CEMO_HO_USERS")),
+    )
+
+    val result = service.getUserCohort(authentication)
+
+    Assertions.assertThat(result.cohort).isEqualTo(Cohort.HOME_OFFICE)
   }
 }
