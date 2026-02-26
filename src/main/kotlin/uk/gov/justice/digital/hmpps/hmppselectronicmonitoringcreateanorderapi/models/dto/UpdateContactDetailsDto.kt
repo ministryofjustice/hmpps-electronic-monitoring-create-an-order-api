93a9c5749e6f832c2c9640bc64b84f0c6e24d098
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2026-02-26 15:50:14 +0000

    Add phone number available flag to contact details

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/dto/UpdateContactDetailsDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/dto/UpdateContactDetailsDto.kt
@@ -1,3 +1,10 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.dto
 
-data class UpdateContactDetailsDto(val contactNumber: String? = null)
+import jakarta.validation.constraints.NotNull
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.data.ValidationErrors
+
+data class UpdateContactDetailsDto(
+  @field:NotNull(message = ValidationErrors.ContactDetails.PHONE_NUMBER_REQUIRED)
+  val phoneNumberAvailable: Boolean? = null,
+  val contactNumber: String? = null,
+)
