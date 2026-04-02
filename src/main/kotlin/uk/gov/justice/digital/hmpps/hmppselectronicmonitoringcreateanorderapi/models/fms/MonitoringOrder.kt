7621dd40046c5d4bfe5ccd123f69c759f32d1b50
aamircodes <aamir.patel@justice.gov.uk>
2026-04-02 14:52:27 +0100

    fix: use notifyingOrg enum

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/fms/MonitoringOrder.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/fms/MonitoringOrder.kt
@@ -671,13 +671,13 @@ data class MonitoringOrder(
           ?: interestedParties.notifyingOrganisationName
           ?: ""
       }
-      val notifyOrg = interestedParties.notifyingOrganisation
+      val notifyOrg = NotifyingOrganisationDDv5.from(interestedParties.notifyingOrganisation)
 
-      if (notifyOrg == NotifyingOrganisationDDv5.PROBATION.name) {
+      if (notifyOrg == NotifyingOrganisationDDv5.PROBATION) {
         return "Probation Board"
       }
 
-      if (notifyOrg == NotifyingOrganisationDDv5.HOME_OFFICE.name) {
+      if (notifyOrg == NotifyingOrganisationDDv5.HOME_OFFICE) {
         return NotifyingOrganisationDDv5.HOME_OFFICE.value
       }
 
