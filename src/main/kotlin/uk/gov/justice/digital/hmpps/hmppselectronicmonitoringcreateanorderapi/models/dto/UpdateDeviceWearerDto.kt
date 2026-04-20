b90ecf385a416fccb8d6014e2e9257a41cc1f46f
James Francis <james.francis@madetech.com>
2026-04-20 11:08:15 +0100

    use correct error message

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/dto/UpdateDeviceWearerDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/dto/UpdateDeviceWearerDto.kt
@@ -15,7 +15,7 @@ data class UpdateDeviceWearerDto(
   @field:Size(max = 200, message = ValidationErrors.DeviceWearer.FIRST_NAME_MAX_LENGTH)
   val firstName: String? = null,
 
-  @field:Size(max = 200, message = ValidationErrors.DeviceWearer.FIRST_NAME_MAX_LENGTH)
+  @field:Size(max = 200, message = ValidationErrors.DeviceWearer.MIDDLE_NAME_MAX_LENGTH)
   val middleName: String? = null,
 
   @field:Size(min = 1, message = ValidationErrors.DeviceWearer.LAST_NAME_REQUIRED)
