35c507dbcc4ce5a87be7816645a0763506be02ba
Bingjie Liu <Bingjie.Liu@justice.gov.uk>
2026-07-01 09:45:25 +0100

    Elm 4746 (#528)
    * Add lastUpdatedBy, lastUpdatedDateTime and ownerCohrot to order version
    
    * Fix flyway script

--- src/main/resources/db/migration/V52__Add_lastUpdateBy_LastUpdateDate_ownerCohrot_To_OrderVersion.sql
+++ src/main/resources/db/migration/V52__Add_lastUpdateBy_LastUpdateDate_ownerCohrot_To_OrderVersion.sql
@@ -1,4 +1,4 @@
 ALTER TABLE order_version
-    ADD COLUMN last_updated_date TIMESTAMP WITHOUT TIME ZONE,
+    ADD COLUMN last_updated_datetime TIMESTAMP WITHOUT TIME ZONE,
     ADD COLUMN owner_cohort VARCHAR(255),
     ADD COLUMN last_updated_by VARCHAR(255);
