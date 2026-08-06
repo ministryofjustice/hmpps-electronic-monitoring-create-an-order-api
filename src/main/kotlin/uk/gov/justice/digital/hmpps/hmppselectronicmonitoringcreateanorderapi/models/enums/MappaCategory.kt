b654b1227e42662fb2846f8569ef85fd20d3986f
James Francis <james.francis@madetech.com>
2026-08-06 10:40:07 +0100

    add mappa enum helpers

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/enums/MappaCategory.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/models/enums/MappaCategory.kt
@@ -4,4 +4,11 @@ enum class MappaCategory(val value: String) {
   CATEGORY_ONE("Category 1"),
   CATEGORY_TWO("Category 2"),
   CATEGORY_THREE("Category 3"),
+  ;
+
+  companion object {
+    fun from(value: String?): MappaCategory? = MappaCategory.entries.firstOrNull {
+      it.value == value
+    }
+  }
 }
