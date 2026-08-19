2386d6155b90d53428354812d190901151eb9fcb
Bingjie Liu <Bingjie.Liu@justice.gov.uk>
2026-08-19 14:33:36 +0100

    Fix flyway script

--- src/main/resources/db/migration/V55__Create_Fms_Order_Details_Table.sql
+++ src/main/resources/db/migration/V55__Create_Fms_Order_Details_Table.sql
@@ -3,5 +3,5 @@ CREATE TABLE fms_order_details
     case_id               VARCHAR(255)            NOT NULL,
     device_wearer_as_json          VARCHAR(409600) NOT NULL,
     monitoring_order_as_json          VARCHAR(409600) NOT NULL,
-    CONSTRAINT pk_fms_order_details PRIMARY KEY (id)
+    CONSTRAINT pk_fms_order_details PRIMARY KEY (case_id)
 );
