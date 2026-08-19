3163d8b5909999302fba27a321078506c3973063
James Francis <james.francis@madetech.com>
2026-08-19 13:55:37 +0100

    small test clean up

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/service/DeviceWearerDetailsServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/service/DeviceWearerDetailsServiceTest.kt
@@ -2,6 +2,8 @@ package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.s
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.client.PrisonerDetailsApi
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.external.hmpps.PrisonerDetails
@@ -43,53 +45,61 @@ class DeviceWearerDetailsServiceTest {
     client = TestClient()
   }
 
-  @Test
-  fun `get details returns bob as first name when set to bob`() {
-    client.setMockResponse(baseDetails.copy(firstName = "Bob"))
+  @Nested
+  @DisplayName("Get details")
+  inner class GetDetails {
+    @Test
+    fun `returns bob as first name when set to bob`() {
+      client.setMockResponse(baseDetails.copy(firstName = "Bob"))
 
-    val service = DeviceWearerDetailsService(client)
+      val service = DeviceWearerDetailsService(client)
 
-    val res = service.getDetailsOverview("1234")
+      val res = service.getDetailsOverview("1234")
 
-    assertThat(res.firstName).isEqualTo("Bob")
+      assertThat(res.firstName).isEqualTo("Bob")
+    }
+
+    @Test
+    fun `returns cat as first name when set to cat`() {
+      client.setMockResponse(baseDetails.copy(firstName = "Cat"))
+
+      val service = DeviceWearerDetailsService(client)
+
+      val res = service.getDetailsOverview("1234")
+
+      assertThat(res.firstName).isEqualTo("Cat")
+    }
+
+    @Test
+    fun `returns basic prison details`() {
+      client.setMockResponse(baseDetails.copy(firstName = "Bob", lastName = "Builder", dateOfBirth = "1990-08-21"))
+
+      val service = DeviceWearerDetailsService(client)
+
+      val res = service.getDetailsOverview("1234")
+
+      assertThat(res.firstName).isEqualTo("Bob")
+      assertThat(res.lastName).isEqualTo("Builder")
+      assertThat(res.dateOfBirth).isEqualTo(
+        ZonedDateTime.of(
+          LocalDateTime.of(1990, 8, 21, 0, 0),
+          ZoneId.of("Europe/London"),
+        ),
+      )
+      assertThat(res.firstName).isEqualTo("Bob")
+    }
   }
 
-  @Test
-  fun `get details returns cat as first name when set to cat`() {
-    client.setMockResponse(baseDetails.copy(firstName = "Cat"))
+  @Nested
+  @DisplayName("Store details")
+  inner class StoreDetails {
+    @Test
+    fun `returns success message`() {
+      val service = DeviceWearerDetailsService(client)
 
-    val service = DeviceWearerDetailsService(client)
+      val response = service.storeDetails("1234")
 
-    val res = service.getDetailsOverview("1234")
-
-    assertThat(res.firstName).isEqualTo("Cat")
-  }
-
-  @Test
-  fun `get details returns basic prison details`() {
-    client.setMockResponse(baseDetails.copy(firstName = "Bob", lastName = "Builder", dateOfBirth = "1990-08-21"))
-
-    val service = DeviceWearerDetailsService(client)
-
-    val res = service.getDetailsOverview("1234")
-
-    assertThat(res.firstName).isEqualTo("Bob")
-    assertThat(res.lastName).isEqualTo("Builder")
-    assertThat(res.dateOfBirth).isEqualTo(
-      ZonedDateTime.of(
-        LocalDateTime.of(1990, 8, 21, 0, 0),
-        ZoneId.of("Europe/London"),
-      ),
-    )
-    assertThat(res.firstName).isEqualTo("Bob")
-  }
-
-  @Test
-  fun `returns success message`() {
-    val service = DeviceWearerDetailsService(client)
-
-    val response = service.storeDetails("1234")
-
-    assertThat(response.success).isEqualTo(true)
+      assertThat(response.success).isEqualTo(true)
+    }
   }
 }
