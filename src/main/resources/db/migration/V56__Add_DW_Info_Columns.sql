2eeebe366cd46d733b9ac302aa991ddc85c49b34
James Francis <james.francis@madetech.com>
2026-08-20 14:30:13 +0100

    create migration for new columns

---
+++ src/main/resources/db/migration/V56__Add_DW_Info_Columns.sql
@@ -0,0 +1,8 @@
+ALTER TABLE device_wearer
+    ADD ethnicity VARCHAR(255);
+
+ALTER TABLE device_wearer
+    ADD nationality VARCHAR(255);
+
+ALTER TABLE device_wearer
+    ADD religion VARCHAR(255);
