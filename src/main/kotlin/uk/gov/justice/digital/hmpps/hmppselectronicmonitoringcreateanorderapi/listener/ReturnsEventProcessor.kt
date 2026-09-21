d0b3003852fe90fb0870c4069f0b63ca64a67ef8
James Francis <james.francis@madetech.com>
2026-09-21 16:35:16 +0100

    extract service

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/listener/ReturnsEventProcessor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/listener/ReturnsEventProcessor.kt
@@ -3,24 +3,11 @@ package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.l
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.enums.OrderStatus
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.external.up3.ReturnMessage
-import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.repository.FmsSubmissionResultRepository
-import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.repository.OrderRepository
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.service.UpdateOrderStatusService
 
 @Service
-class ReturnsEventProcessor(
-  private val submissionRepo: FmsSubmissionResultRepository,
-  private val repo: OrderRepository,
-) {
-
+class ReturnsEventProcessor(private val updateOrderStatusService: UpdateOrderStatusService) {
   fun onRejected(message: ReturnMessage) {
-    // get order version by case id
-    val submissionResult =
-      submissionRepo.findByCaseId(message.caseId)
-        ?: throw Exception("Submission result does not exist for case id: ${message.caseId}")
-    val order = repo.findById(submissionResult.orderId).get()
-
-    // update order status to be failed
-    order.status = OrderStatus.REJECTED
-    repo.save(order)
+    updateOrderStatusService.execute(message.caseId, OrderStatus.REJECTED)
   }
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/service/FindOrderByCaseIdService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/service/FindOrderByCaseIdService.kt
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.Order
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.dto.OrderCaseSearchResultDto
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.dto.toOrderVersionDto
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.enums.OrderStatus
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.repository.OrderRepository
 
 interface OrderByCaseIdGateway {
   fun findOrderByCaseId(caseId: String): Order?
@@ -13,5 +15,16 @@ interface OrderByCaseIdGateway {
 class FindOrderByCaseIdService(private val gateway: OrderByCaseIdGateway) {
   fun execute(caseId: String): OrderCaseSearchResultDto? = gateway.findOrderByCaseId(caseId)?.let {
     OrderCaseSearchResultDto(id = it.id, versions = it.versions.map { version -> version.toOrderVersionDto() })
+  }
+}
+
+@Service
+class UpdateOrderStatusService(private val gateway: OrderByCaseIdGateway, private val repo: OrderRepository) {
+  fun execute(caseId: String, status: OrderStatus) {
+    val order = gateway.findOrderByCaseId(caseId)
+
+    order!!.status = status
+
+    repo.save(order)
   }
 }
