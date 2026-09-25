beda0dfe752b13809cf25fadd7873dd71e06f953
James Francis <james.francis@madetech.com>
2026-09-25 16:09:42 +0100

    make service transactional

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/service/RejectOrderService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringcreateanorderapi/service/RejectOrderService.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.service
 
 import jakarta.persistence.EntityNotFoundException
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.models.RejectionReason
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringcreateanorderapi.repository.OrderRepository
@@ -8,6 +9,7 @@ import java.time.ZonedDateTime
 
 @Service
 class RejectOrderService(private val gateway: OrderByCaseIdGateway, private val repo: OrderRepository) {
+  @Transactional
   fun execute(caseId: String, dateTime: ZonedDateTime, reasons: List<RejectionReason>) {
     val order = gateway.findOrderByCaseId(caseId)
       ?: throw EntityNotFoundException("Order with caseId $caseId does not exist")
